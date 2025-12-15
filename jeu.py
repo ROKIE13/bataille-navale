54f35949fe008047a8cafd69390a876b9341d732
ROKIE13 <boubaamin2008@gmail.com>
2025-12-15 11:57:20 +0100

    Update jeu.py

--- jeu.py
+++ jeu.py
@@ -1,26 +1,39 @@
+import random
+
 class Joueur:
-    def __init__(self, couleur):
-        self.couleur = couleur
+    def __init__(self, id):
+        self.id = id
+        self.issue = False
         self.score = 0
         self.grillePerso = Grille()
         self.grilleAdv = Grille()
-    def remplissage(self):
-        bateauxPerso = []
         for i in range(6):
-            #ligne ou l on place dans l'interface les coordonnées x et y du bateau/case
-            bateau = Bateau()
-            bateauxPerso.append(bateau)
+            bateau = Bateau(random.randint(0, 9), random.randint(0, 9), random('H', 'V'), random.randint(1, 5))
+            for j in range(0, len(bateau.pos()), 2):
+                self.grillePerso[bateau.pos[j]][bateau.pos[j+1]] = "R"
 
-    def tour_de_jeu(self):
-        x = 
-        y = 
-        if #touche:
-            if #coulé:
-            self.score +=1
-        else:
-        
-        
-    
-    def a_gagne(self):
+            
+                
+
+    def tour_de_jeu(self, other):
+        x = int(input("Choisir une colonne horizontale entre 1 et 10")) - 1
+        y = int(input("Choisir une colonne verticale entre 1 et 10")) - 1
+        self.grilleAdv[x][y] = "X"
+        for i in range(6):
+            other.bateaux[i].est_touche(x, y)
+        coulement = []
+        self.score = 0
+        for i in range[6]:
+            coulement.append(other.bateaux[i].est_coule)
+            if coulement[i]:
+                self.score += 1
         if self.score == 6:
+            return True
+        return False
 
+joueur1 = Joueur(input("Quel est ton nom ?"))
+joueur2 = Joueur(input("Quel est ton nom ?"))
+while not joueur1.issue and not joueur2.issue:
+    joueur1.tour_de_jeu(joueur2)
+    if not joueur1.issue:
+        joueur2.tour_de_jeu(joueur1)
