b55102ae3a373c476dc45a08e895bca52000d87f
Omarelm27 <omarelmekkaoui5d@gmail.com>
2025-12-15 10:42:36 +0100

    Grille.py

--- Grille.py
+++ Grille.py
@@ -3,7 +3,12 @@ class Grille:
         self.largeur=10
         self.longueur=10
         self.tab=[[None for j in range(10)] for i in range(10)]
-        if self.longueur*self.largeur<=0 :
-            return print("Le tableau doit être positif")
-        
+        for i in range (len(self.tab)):
+            table = ''
+            for j in range(len(self.tab[i])):
+                table += " " + str(self.tab[i][j])
+            print(table)
+
+uu = Grille()
+
 
